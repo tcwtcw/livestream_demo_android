6f930350e56d8a0fc80c9e5a62eba0496bb2c259
tcwtcw <675807942@qq.com>
2017-03-01 20:14:01 +0800

    直播列表的Tab页修改为热门和最新,需要添加排序,todo

--- app/src/main/java/cn/ucai/live/ui/activity/LiveSquareFragment.java
+++ app/src/main/java/cn/ucai/live/ui/activity/LiveSquareFragment.java
@@ -59,8 +59,7 @@ public class LiveSquareFragment extends Fragment{
     private void setupViewPager(){
         Adapter adapter = new Adapter(getActivity().getSupportFragmentManager());
         adapter.addFragment(new LiveListFragment(), "热门");
-        adapter.addFragment(new LiveListFragment(), "女神");
-        adapter.addFragment(new LiveListFragment(), "男神");
+        adapter.addFragment(new LiveListFragment(), "最新");
         viewPager.setAdapter(adapter);
     }
 
