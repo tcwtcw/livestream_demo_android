4f832092130c0b9ffd2c8f864c3985c31c76e0e6
tcwtcw <675807942@qq.com>
2017-03-06 19:53:17 +0800

    我的钱包数据加载

--- app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
+++ app/src/main/java/cn/ucai/live/ui/activity/ChangeActivity.java
@@ -7,9 +7,18 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.hyphenate.chat.EMClient;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import cn.ucai.live.R;
+import cn.ucai.live.data.NetDao;
+import cn.ucai.live.data.model.Result;
+import cn.ucai.live.data.model.Wallet;
+import cn.ucai.live.utils.CommonUtils;
+import cn.ucai.live.utils.OnCompleteListener;
+import cn.ucai.live.utils.PreferenceManager;
+import cn.ucai.live.utils.ResultUtils;
 
 /**
  * Created by clawpo on 2017/3/6.
@@ -21,6 +30,8 @@ public class ChangeActivity extends BaseActivity {
     LinearLayout mTargetLayout;
     View loadingView;
 
+    int change;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,5 +40,42 @@ public class ChangeActivity extends BaseActivity {
         loadingView = LayoutInflater.from(ChangeActivity.this)
                 .inflate(R.layout.rp_loading, mTargetLayout,false);
         mTargetLayout.addView(loadingView);
+        setChange();
+        initData();
+    }
+
+    private void initData() {
+        NetDao.loadChange(ChangeActivity.this, EMClient.getInstance().getCurrentUser(),
+                new OnCompleteListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        boolean success = false;
+                        if (s!=null){
+                            Result result = ResultUtils.getResultFromJson(s, Wallet.class);
+                            if (result!=null && result.isRetMsg()){
+                                success = true;
+                                Wallet wallet = (Wallet) result.getRetData();
+                                PreferenceManager.getInstance().setCurrentuserChange(wallet.getBalance());
+                                change = wallet.getBalance();
+                                setChange();
+                            }
+                        }
+                        if (!success){
+                            PreferenceManager.getInstance().setCurrentuserChange(0);
+                        }
+                        loadingView.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onError(String error) {
+                        loadingView.setVisibility(View.GONE);
+                        CommonUtils.showShortToast(error);
+                    }
+                });
+    }
+
+    private void setChange() {
+        change = PreferenceManager.getInstance().getCurrentuserChange();
+        mTvChangeBalance.setText("￥" + Float.valueOf(String.valueOf(change)));
     }
 }
